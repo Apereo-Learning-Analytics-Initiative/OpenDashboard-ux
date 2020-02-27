4aa2be7812246cf7caec2074eb168b87a9c9c700
Xavier Chopin <xchopin54@gmail.com>
2020-02-27 19:17:11 +0100

    feat(eventAlias): improve manager

--- src/view/admin/alias/EventAliasManager.js
+++ src/view/admin/alias/EventAliasManager.js
@@ -1,12 +1,18 @@
-import React from 'react';
+/**
+ * @author xchopin <bonjour@chopin.us>
+ * @author scody
+ */
+
+import React, {useState} from 'react';
 import { Route, Link, withRouter, useParams } from 'react-router-dom';
 import findIndex from 'lodash/findIndex';
-import { useTenantAliasList } from '../../../service/Tenant';
-import { Button } from 'reactstrap';
+import {Button, Modal, ModalBody, ModalHeader} from 'reactstrap';
 import EditAlias from './EditAlias';
-import AddAlias from './AddAlias';
-import { create, update, remove } from '../../../service/Alias';
+import {create, update, remove, useFindAll} from '../../../service/Alias';
 import { useTenantAliasUrl, useTenantAliasDeleteUrlFn } from '../../../shared/path';
+import AliasForm from './AliasForm';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheck, faCross, faPen, faPlus, faTimes, faTrash, faWindowClose} from "@fortawesome/free-solid-svg-icons";
 
 
 function EventAliasManager({ match }) {
@@ -15,80 +21,160 @@ function EventAliasManager({ match }) {
     const tenantId = params.tenantId;
     const url = useTenantAliasUrl(tenantId);
     const deleteUrl = useTenantAliasDeleteUrlFn();
-    
     const defaultAlias = { tenantId };
-
-    const aliases = useTenantAliasList(tenantId);
     const redirectPath = `${match.url}`;
-
-    const [list, setList] = React.useState([]);
+    const [list, setList] = React.useState(useFindAll(tenantId));
     const [newAlias, setNewAlias] = React.useState({ ...defaultAlias });
+    const [modal, setModal] = useState(false);
+    const [modalTitle, setModalTitle] = useState(null);
+    const [modalBody, setModalBody] = useState(false);
+    const toggle = () => setModal(!modal);
+    const closeBtn = <button className="close" onClick={toggle}>&times;</button>;
+
+    /**
+     * Show a modal window with the form
+     * @param title
+     * @param body
+     */
+    const showModal = (title, body) => {
+        toggle();
+        setModalTitle(title);
+        setModalBody(body);
+    };
+
 
     React.useEffect(() => {
-        setList(aliases);
-    }, [aliases]);
+        setList(list);
+    }, [list]);
 
-    const addNewAlias = (data) => {
+    /**
+     * Persist the new alias into the API
+     * @param data
+     */
+    const saveAlias = (data) => {
         create(url, data).then((resp) => {
-            const list = [ ...aliases ];
-            list.push(resp);
-            setList(list);
+            const newList = [ ...list ];
+            newList.push(resp);
+            setList(newList);
             setNewAlias({ ...defaultAlias });
-            window.location.reload();
         });
+
+        setModal(false);
     };
 
+    /**
+     * Update the alias into the API
+     * @param data
+     */
     const updateAlias = (data) => {
-        update(url, data).then(resp => {
-            const list = [ ...aliases ];
-            const indexToUpdate = findIndex(list, (a) => a.id === data.id);
-            const old = list[indexToUpdate];
-            list[indexToUpdate] = { ...old, ...data };
-            setList(list);
-            window.location.reload();
+        update(url, data).then((resp) => {
+            const newList = [ ...list ];
+            const indexToUpdate = findIndex(newList, (a) => a.id === data.id);
+            const old = newList[indexToUpdate];
+            newList[indexToUpdate] = { ...old, ...data };
+            setList(newList);
         })
+
+        setModal(false);
     };
+
+
+
+    /**
+     * Display the modal window with the 'add new alias' form
+     */
+    const showNewForm = () => {
+        const data = { tenantId };
+        const body =  <AliasForm data={data} onSubmit={ saveAlias } />;
+        showModal('Add an alias', body)
+    };
+
+
+    const showEditForm = (itemId) => {
+        const body = <EditAlias onSubmit={ updateAlias }  data={ list.find(a => a.id === itemId) } />
+        showModal('Edit "' + itemId + '"' , body);
+    };
+
+
 
     const deleteAlias = (data) => {
         remove(deleteUrl(data.id), data).then(resp => {
-            setList(aliases.filter(a => a.id !== data.id));
+            setList(list.filter(a => a.id !== data.id));
         });
     };
 
+
+
     return (
         <React.Fragment>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Verb</th>
-                        <th>Alias</th>
-                        <th>Display</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {list.map((a, index) =>
-                    <tr key={index}>
-                        <td className="text-break">{a.verb}</td>
-                        <td className="text-break">{a.alias}</td>
-                        <td>{a.display ? 'true' : 'false'}</td>
-                        <td className="btn-group">
-                            <Link className="btn btn-primary btn-sm" to={ `${match.url}/${a.id}` } >Edit</Link>
-                            <Button color="danger" size="sm" onClick={ () => deleteAlias(a) }>Delete</Button>
-                        </td>
-                    </tr>
-                    )}
-                </tbody>
-            </table>
-            <Route path={`${match.path}`} exact render={
-                ({ match }) => <AddAlias data={ newAlias } onSubmit={ addNewAlias } />
-            } />
-            <Route path={`${match.path}/:alias`} render={
-                ({ match }) => <EditAlias onSubmit={ updateAlias } redirectPath={redirectPath} data={ aliases.find(a => a.id === match.params.alias) } />
-            } />
+            <button className="btn btn-outline-primary btn-block" onClick={showNewForm } >
+                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon> &nbsp;
+                Add an event alias
+            </button>
+            <br/>
+
+            { list.length < 1  ? <h6>There is no event alias available.</h6> :
+                <div className="list-aliases">
+                    <hr/>
+                    <br/>
+                    <span className="section-title">Existing Aliases</span>
+
+
+                    <br/>
+                    <br/>
+                    <table className="table">
+                        <thead className="thead-dark">
+                            <tr>
+                                <th>Verb</th>
+                                <th>Alias</th>
+                                <th>Display</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {list.map((item, index) =>
+                            <tr key={index}>
+                                <td id={item.verb} className="text-break">{item.verb}</td>
+                                <td className="text-break">{item.alias}</td>
+                                <td>{
+                                    item.display ?
+                                        <FontAwesomeIcon color='green' icon={faCheck}></FontAwesomeIcon>
+                                        :
+                                        <FontAwesomeIcon color='red' icon={faTimes}></FontAwesomeIcon> }</td>
+                                <td>
+                                    <button className="btn btn-lg btn-primary btn-sm" onClick={ () => showEditForm(item.id)}>
+                                        <FontAwesomeIcon icon={faPen}></FontAwesomeIcon> &nbsp;
+                                        Edit
+                                    </button> &nbsp;
+                                    <button className="btn btn-lg btn-danger btn-sm" onClick={ () => deleteAlias(item) }>
+                                        <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon> &nbsp;
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            }
+
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle} close={closeBtn}>
+                    { modalTitle }
+                </ModalHeader>
+                <ModalBody>
+                    { modalBody }
+                </ModalBody>
+            </Modal>
         </React.Fragment>
-        
     );
 }
+
+//            <Route path={`${match.path}`} exact render={
+//                 ({ match }) => <AddAlias data={ newAlias } onSubmit={ addNewAlias } />
+//             } />
+//             <Route path={`${match.path}/:alias`} render={
+//                 ({ match }) => <EditAlias onSubmit={ updateAlias } redirectPath={redirectPath} data={ aliases.find(a => a.id === match.params.alias) } />
+//             } />
 
 export default withRouter(EventAliasManager);
