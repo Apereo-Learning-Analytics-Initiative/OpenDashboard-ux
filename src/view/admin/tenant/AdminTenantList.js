77e6862004c0192a4c05e9deb89d62b58aae5648
Xavier Chopin <xchopin54@gmail.com>
2020-02-28 22:22:27 +0100

    feat: improve ui on tenant manager

--- src/view/admin/tenant/AdminTenantList.js
+++ src/view/admin/tenant/AdminTenantList.js
@@ -2,26 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { useTenantList } from '../../../service/Tenant';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
 
 function AdminTenantList() {
 
-    const tenantlist = useTenantList();
+    const tenantList = useTenantList();
 
     return (
-        <div className="d-flex justify-content-center mt-2">
-            <div className="w-33">
-                <div className="list-group">
-                    {tenantlist.map(tenant =>
-                        <Link key={tenant.id} to={`/admin/tenant/${tenant.id}`} className="list-group-item list-group-item-action">
-                            {tenant.name}
-                        </Link>
-                    )}
+        <div className="container">
+            <div className="mt-4">
+                    <div className='mb-4'>
+                        <span className='header-title'>My tenants</span>
+                            <Link to={`/admin/tenants/new`} >
+                                <button className='float-right btn btn-outline-primary'>
+                                    <FontAwesomeIcon icon={faPlus} /> &nbsp;
+                                    Add a new tenant
+                                </button>
+                            </Link>
+                    </div>
+                    <hr/>
+                    { tenantList.length < 1  ?
+                        <h5>There is no tenants yet, please create a new one.</h5>
+                        :
+                       <div className="list-group">
+                          {tenantList.map(tenant =>
+                              <Link key={tenant.id} to={`/admin/tenant/${tenant.id}`} className="list-group-item list-group-item-action">
+                                  {tenant.name}
+                              </Link>
+                          )}
+
+                       </div>
+                    }
+
                 </div>
-                <hr />
-                <div className="text-center">
-                    <Link to={`/admin/tenants/new`} className="">Add a new tenant</Link>
-                </div>
-            </div>
         </div>
         
     );
